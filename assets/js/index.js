811702821800c56d9a54d79c237dcee0c4a8bf68
Jair Torezone <jairtorezone@gmail.com>
2022-03-15 19:12:38 -0300

    Pokedex - add index.js

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//Criar duas variáveis com os elementos da tela
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon');
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon');
+
+
+listaSelecaoPokemons.forEach(pokemon => {
+    //Vamos precisar trabalhar com um evento de click pelo usuário na listagem pokemon
+    pokemon.addEventListener('click', () => {
+
+    })
+});
